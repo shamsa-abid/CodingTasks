902fd8179511944ee7790f2cfc0a4422d5bcee8c
phdstudent15030049 <15030049@lums.edu.pk.com>
2018-11-30 09:57:14 +0500

    Walking over all commits in App.java

--- task1/src/main/java/refdiff/task1/App.java
+++ task1/src/main/java/refdiff/task1/App.java
@@ -1,12 +1,16 @@
 package refdiff.task1;
 
+import java.util.Collection;
 /**
  * Hello world!
  *
  */
 import java.util.List;
 
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 
 import refdiff.core.RefDiff;
 import refdiff.core.api.GitService;
@@ -17,11 +21,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         RefDiff refDiff = new RefDiff();
         GitService gitService = new GitServiceImpl();
-        try (Repository repository = gitService.cloneIfNotExists("F:/tmp/jersey", "https://github.com/jersey/jersey.git")) {
-            List<SDRefactoring> refactorings = refDiff.detectAtCommit(repository, "d94ca2b27c9e8a5fa9fe19483d58d2f2ef024606");
+        try (Repository repository = gitService.cloneIfNotExists("F:/tmp/dp", "https://github.com/iluwatar/java-design-patterns.git")) {
+        	
+            List<SDRefactoring> refactorings = refDiff.detectAtCommit(repository, "2aa9e78dddb410b48f6d06c3ea6de1b8f6db4ffc");
             for (SDRefactoring r : refactorings) {
                 System.out.printf("%s\t%s\t%s\n", r.getRefactoringType().getDisplayName(), r.getEntityBefore().key(), r.getEntityAfter().key());
             }
+            
+            Collection<Ref> allRefs = repository.getAllRefs().values();
+
+	         // a RevWalk allows to walk over commits based on some filtering that is defined
+	         try (RevWalk revWalk = new RevWalk( repository )) {
+	             for( Ref ref : allRefs ) {
+	                 revWalk.markStart( revWalk.parseCommit( ref.getObjectId() ));
+	             }
+	             System.out.println("Walking all commits starting with " + allRefs.size() + " refs: " + allRefs);
+	             int count = 0;
+	             for( RevCommit commit : revWalk ) {
+	                 System.out.println("Commit: " + commit);
+	                 count++;
+	             }
+	             System.out.println("Had " + count + " commits");
+	         }
         }
     }
 }
