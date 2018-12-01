b10e2dcc1e6b72f0c5855e9e0f8bfbcf37dfd9cd
phdstudent15030049 <15030049@lums.edu.pk.com>
2018-12-01 17:50:40 +0500

    pushing execuatble Task 1

--- task1/src/main/java/refdiff/task1/App.java
+++ task1/src/main/java/refdiff/task1/App.java
@@ -22,18 +22,30 @@ import refdiff.core.util.GitServiceImpl;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-
-
+		
+		String repoURL = "https://github.com/shamsa-abid/CodingTasks.git";
+		//String repoURL = "https://github.com/danilofes/refactoring-toy-example.git";
+		//String outputFile = "output/outputRenameMethodRefactorings.csv";
+		String outputFile = "output/outputChangeSignatureRefactoringsCodingTasks.csv";
+		String cloneDir = "tmp/codingtaskexample";
+				
+		if(args.length > 0)
+		{
+			repoURL = args[0];
+			outputFile = args[1];
+			cloneDir = args[2];
+		}
+		
 		RefDiff refDiff = new RefDiff();
 		GitService gitService = new GitServiceImpl();
 
 		
-		File file = new File("output/output.csv");
-		FileWriter fr = new FileWriter(file, true);
+		File file = new File(outputFile);
+		FileWriter fr = new FileWriter(file);
 		BufferedWriter br = new BufferedWriter(fr);
 		br.write("Commit SHA, Java File, Old function signature, New function signature\n");
 
-		try (Repository repository = gitService.cloneIfNotExists("F:/tmp/mycode", "https://github.com/shamsa-abid/CodingTasks.git")) {
+		try (Repository repository = gitService.cloneIfNotExists(cloneDir, repoURL)) {
 
 
 			Collection<Ref> allRefs = repository.getAllRefs().values();
@@ -43,25 +55,31 @@ public class App {
 				for( Ref ref : allRefs ) {
 					revWalk.markStart( revWalk.parseCommit( ref.getObjectId() ));
 				}
-				System.out.println("Walking all commits starting with " + allRefs.size() + " refs: " + allRefs);
+				//System.out.println("Walking all commits starting with " + allRefs.size() + " refs: " + allRefs);
 				int count = 0;
+				int refactoringCount = 0;
+				int addParamPefactoringCount = 0;
+				
 				for( RevCommit commit : revWalk ) {
-					System.out.println("Commit: " + commit);
+					//System.out.println("Commit: " + commit);
 
 					List<SDRefactoring> refactorings = refDiff.detectAtCommit(repository, commit.name());
 					for (SDRefactoring r : refactorings) {
-						if(r.getRefactoringType().name() == "RENAME_METHOD" )
+						refactoringCount ++;
+						
+						if(r.getRefactoringType().name() == "CHANGE_METHOD_SIGNATURE" )
 						{
+							addParamPefactoringCount ++;
 							System.out.printf("%s\t%s\t%s\n", r.getRefactoringType().getDisplayName(), r.getEntityBefore().key(), r.getEntityAfter().key());
 							String before = r.getEntityBefore().key().toString();
 							String after = r.getEntityAfter().key().toString();
 							String javaFile = before.substring(0, before.indexOf("#"));
 							String oldSign = before.substring(before.indexOf("#") + 1);
 							String newSign = after.substring(after.indexOf("#") + 1);
-
-
-							br.write(commit.name() + "," + javaFile + "," + oldSign + "," + newSign);
+							br.write(commit.name() + "," + javaFile + "," + oldSign + "," + newSign+ "\n");
 						}
+						//else
+							//System.out.println("Method signature change refactoring not detected");
 					}
 
 					count++;
@@ -69,6 +87,8 @@ public class App {
 				br.close();
 				fr.close();
 				System.out.println("Had " + count + " commits");
+				System.out.println("Had " + refactoringCount + " various refactorings");
+				System.out.println(addParamPefactoringCount + " add parameter refactorings detected");
 			}
 		}
 	}
